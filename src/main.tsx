c1ae1d5274da478fceb22b2ffe51674db557b6b6
Fırat YILDIZ <firatyildiznet@gmail.com>
2024-03-01 15:41:14 +0300

    Added material npm packages and settings

--- src/main.tsx
+++ src/main.tsx
@@ -8,17 +8,21 @@ import "@fontsource/open-sans/600.css";
 
 import './index.css'
 
+import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 
 import store from './redux/store.ts';
+import theme from './theme.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.Fragment>
-     <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
+     <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </Provider>
+    </ThemeProvider>
   </React.Fragment>,
 )
---
+++ src/theme.tsx
@@ -0,0 +1,19 @@
+import { createTheme } from '@mui/material/styles';
+
+// A custom theme for this app
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#FFFFFF',
+      contrastText: 'green',
+    },
+    error: {
+      main: '#ff3f55'
+    }
+  }
+});
+
+export default theme;
