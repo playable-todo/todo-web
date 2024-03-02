73274eead99cdbd869df14dc3caf445cb16a9725
Fırat YILDIZ <firatyildiznet@gmail.com>
2024-03-02 19:07:26 +0300

    Edited TodoSection component

--- src/components/TodoSection.tsx
+++ src/components/TodoSection.tsx
@@ -1,10 +1,13 @@
 import React, { useState, Fragment } from 'react'
 
 import { 
+    Box,
     Grid,
     Card,
     CardContent,
     CardActions,
+    Typography,
+    Avatar
 } from '@mui/material';
 
 
@@ -12,22 +15,33 @@ import {
 import { TodoSectionProp } from './component';
 
 const TodoSection: React.FC<TodoSectionProp> = ({ data }) => {
+    const EndPoint = import.meta.env.VITE_ENDPOINT;
 
     return (
-        <>
+        <Grid container spacing={3}>
             {data.length > 0 && data.map((item, key) => (
                 <Grid item={true} lg={6} xl={6} md={6} sm={6} xs={12} key={key}>
                     <Card variant="outlined">
                             <CardContent>
-                            
+                                <Typography sx={{ fontSize: '16px', fontWeight: 600, marginBottom: 1 }}>
+                                     {item.title}
+                                </Typography>
+                                <Typography sx={{ fontSize: '16px', marginBottom: 1 }}>
+                                     {item.content}
+                                </Typography>
+                                {item?.image !== null  && (
+                                    <Box sx={{ display: 'inline-flex' }}>
+                                        <Avatar 
+                                            src={EndPoint + item?.image?.url}  
+                                            sx={{ width: 40, marginRight: '5px', border: '2px solid #7c4b00' }}
+                                        />
+                                    </Box>
+                                )}
                             </CardContent>
-                            <CardActions disableSpacing sx={{ display: 'block', backgroundColor: '#f4f4f4' }}>
-
-                            </CardActions>
                     </Card>
                 </Grid>
             ))}
-        </>
+        </Grid>
     )
 
 }
