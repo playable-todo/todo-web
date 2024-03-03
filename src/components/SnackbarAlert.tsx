78c92d6a0fa46bde74186b0ee0a4d6b6330e7de9
Fırat YILDIZ <firatyildiznet@gmail.com>
2024-03-03 19:56:10 +0300

    Edited SnackbarAlert for clean code

--- src/components/SnackbarAlert.tsx
+++ src/components/SnackbarAlert.tsx
@@ -1,24 +1,30 @@
 
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect} from 'react';
+
+// Material UI elements
 import { Snackbar, Alert } from '@mui/material';
 
+// Interfaces
 import { snackBarProps, snackbarOptionsProps } from './component';
 
 export const SnackbarAlert: React.FC<snackBarProps> = ({snackbarOptions}) => {
+
+  // useStates
   const [open, setOpen] = useState(false);
   const [options, setOptions] = useState<snackbarOptionsProps>({})
   
+  // useEffects
   useEffect(() => {
-    setOpen(true);
-    setOptions(snackbarOptions!)
-}, [snackbarOptions])
+      setOpen(true);
+      setOptions(snackbarOptions!)
+  }, [snackbarOptions])
 
   const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
-    if (reason === 'clickaway') {
-      return;
-    }
+      if (reason === 'clickaway') {
+        return;
+      }
 
-    setOpen(false);
+      setOpen(false);
   };
 
   return (
@@ -30,12 +36,19 @@ export const SnackbarAlert: React.FC<snackBarProps> = ({snackbarOptions}) => {
     >
         <Alert
             onClose={handleClose}
-            severity={options.type == "success" ? "success" : options.type == "error" ? "error" : options.type == "info" ? "info" : "warning"} 
+            severity={
+                options.type == "success" ? 
+                  "success" : 
+                options.type == "error" ? 
+                  "error" : 
+                options.type == "info" ? 
+                "info" : "warning"
+            } 
             variant="filled"
             sx={{ width: '100%' }}
         >
             {options.message}
         </Alert>
-  </Snackbar>
+    </Snackbar>
   );
 }
